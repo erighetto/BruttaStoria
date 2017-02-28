c359e968829a7ec75a1719b91c3f2463369be39f
Emanuel Righetto <posta@emanuelrighetto.it>
2017-02-28 21:22:39 +0100

    sitemap

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PageController extends Controller
 {
+
     public function manifestoAction()
     {
         return $this->render('page/manifesto.html.twig', array(
